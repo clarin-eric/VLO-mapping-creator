fca174de3de327618566a49a031ffa9c098fe474
Menzo Windhouwer <menzo@windhouwer.nl>
2018-01-30 08:04:51 +0100

    Added support for the template. Some work on debugging.

--- src/main/java/eu/clarin/vlo/mapping/Creator.java
+++ src/main/java/eu/clarin/vlo/mapping/Creator.java
@@ -2,10 +2,8 @@ package eu.clarin.vlo.mapping;
 
 import java.io.File;
 import java.io.OutputStreamWriter;
-import static java.lang.System.out;
 import java.nio.file.Files;
 import java.util.List;
-import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
@@ -15,6 +13,7 @@ import net.sf.saxon.s9api.XdmAtomicValue;
 import net.sf.saxon.s9api.XsltExecutable;
 import net.sf.saxon.s9api.XsltTransformer;
 import nl.mpi.tla.util.Saxon;
+import nl.mpi.tla.util.saxon.Listener;
 import org.slf4j.LoggerFactory;
 
 /*
@@ -51,13 +50,19 @@ public class Creator {
     private static void help() {
         System.err.println("INF: java -jar vlo-mapping-creator.jar <OPTION>* <CSV>, where <OPTION> is one of those:");
         System.err.println("INF: -s=<SKOS> SKOS file to merge with the CSV");
+        System.err.println("INF: -t=<TMPL> Template file to merge with the Mapping XML");
+        System.err.println("INF: -d        Enable debug info");
     }
 
     public static void main(String[] args) {
+        Boolean debug = false;
         File skos = null;
+        File tmpl = null;
+        File tmp = null;
 
-        OptionParser parser = new OptionParser("s:?*");
+        OptionParser parser = new OptionParser("s:t:?*");
         OptionSet options = parser.parse(args);
+
         if (options.has("s")) {
             skos = new File((String)options.valueOf("s"));
             if (!skos.canRead()) {
@@ -65,6 +70,17 @@ public class Creator {
                 System.exit(1);
             }
         }
+
+        if (options.has("t")) {
+            tmpl = new File((String)options.valueOf("t"));
+            if (!tmpl.canRead()) {
+                LOGGER.error("Can't read the template file["+tmpl+"]!");
+                System.exit(1);
+            }
+        }
+
+        debug = options.has("d");
+
         if (options.has("?")) {
             help();
             System.exit(0);
@@ -82,24 +98,39 @@ public class Creator {
         }
         
         try {
+            Listener l = new Listener("VLO-mapping-creator");
+
             if (skos!=null) {
                 XsltTransformer tf = TO_CSV.load();
+                tf.setMessageListener(l);
+                tf.setErrorListener(l);
                 tf.setParameter(new QName("csv"), new XdmAtomicValue("file:"+csv.getAbsolutePath().toString()));
                 tf.setSource(new StreamSource(skos));
                 Serializer s = new Serializer();
-                csv = Files.createTempFile("vlo-mapping-create-",".csv").toFile();
+                tmp = Files.createTempFile("vlo-mapping-create-",".csv").toFile();
+                csv = tmp;
                 s.setOutputFile(csv);
                 tf.setDestination(s);
                 tf.transform();
                 LOGGER.debug("Created CSV["+csv+"]");
             }
             XsltTransformer tf = TO_XML.load();
+            tf.setMessageListener(l);
+            tf.setErrorListener(l);
             tf.setParameter(new QName("csv"), new XdmAtomicValue("file:"+csv.getAbsolutePath().toString()));
+            if (tmpl!=null)
+                tf.setParameter(new QName("defaults"), Saxon.buildDocument(new StreamSource(tmpl)));
             tf.setInitialTemplate(new QName("main"));
             Serializer s = new Serializer();
             s.setOutputWriter(new OutputStreamWriter(System.out, "UTF-8"));
             tf.setDestination(s);
             tf.transform();
+            
+            if (!debug && tmp!=null) {
+                tmp.delete();
+                LOGGER.debug("Removed CSV["+csv+"]");
+            }
+            
         } catch(Throwable t) {
             LOGGER.error("Failed to create the VLO mapping!", t);
             System.exit(1);
